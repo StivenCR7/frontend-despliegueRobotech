175dd831abc1322eaa9bcb1bedc3019f5725bb28
StivenCR7 <stivengg234@gmail.com>
2025-02-25 12:27:30 -0500

    Update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -3,9 +3,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: 
    "https://torneorobotech-dzawctb0axeggxe7.brazilsouth-01.azurewebsites.net", //URL api
-    
-  // Agregamos una propiedad personalizada para la URL de las imágenes
-  imageURL : "https://storagetorneorobotech.blob.core.windows.net/imagenes/"
 });
 
 // Interceptor para agregar el token en cada solicitud
