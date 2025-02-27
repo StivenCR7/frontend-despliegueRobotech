ebf14a02327f893739493bfc122a13202d4aef21
StivenCR7 <stivengg234@gmail.com>
2025-02-26 21:39:14 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true, // Esto redirige todas las rutas a index.html para que React Router las maneje
-  },
+  build: {
+    outDir: "dist", // Asegurar que el build se genera en 'dist/'
+  }
 });
