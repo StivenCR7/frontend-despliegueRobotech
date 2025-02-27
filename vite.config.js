5d258eae31db8b29e723a05d2d8a39b26cc1cf6a
StivenCR7 <stivengg234@gmail.com>
2025-02-26 21:19:59 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Asegura que las rutas se resuelvan correctamente
   plugins: [react()],
   server: {
     historyApiFallback: true, // Esto redirige todas las rutas a index.html para que React Router las maneje
